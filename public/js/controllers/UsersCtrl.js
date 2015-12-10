62043a4043daacab4b4c6a3d677988b1aad82a5e
annieredmond1 <annieredmond1@gmail.com>
2015-12-09 16:39:29 -0800

    user seed data in factory

--- public/js/controllers/UsersCtrl.js
+++ public/js/controllers/UsersCtrl.js
@@ -3,8 +3,10 @@
 /* USER Controllers */
 
 angular.module('walkr-fit')
-  .controller('ProfileCtrl', ['$scope', '$http', '$auth', 'Auth', function($scope, $http, $auth, Auth) {
+  .controller('ProfileCtrl', ['$scope', '$http', '$auth', 'Auth', 'Users', function($scope, $http, $auth, Auth, Users) {
     $http.get('/api/me').then(function(data) {
       $scope.user = data.data;
     });
+
+    $scope.users = Users.all();
   }]);
--- public/js/services.js
+++ public/js/services.js
@@ -4,7 +4,7 @@
 /* Services */
 
 angular.module('walkr-fit.services', [])
-
+  //get the current user
   .factory('Auth', ['$auth', function ($auth) {
     return {
       currentUser: function() {
@@ -14,8 +14,50 @@ angular.module('walkr-fit.services', [])
           email: user.email,
           picture: user.picture,
           displayName: user.displayName
-        }
+        };
         return currentUser;
       }
-    }
+    };
   }])
+  //seed data
+  .factory('Users', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var users = [{
+    id: 1,
+    first: 'Ben',
+    last: 'Smith',
+    username: 'bsmith',
+    email: 'b@b.com',
+  }, {
+    id: 2,
+    first: 'Mary',
+    last: 'Smith',
+    username: 'marylittle',
+    email: 'm@m.com',
+  }, {
+    id: 3,
+    first: 'Suzie',
+    last: 'Smith',
+    username: 'suze',
+    email: 's@a.com',
+  }];
+
+  return {
+    all: function() {
+      return users;
+    },
+    remove: function(user) {
+      users.splice(users.indexOf(user), 1);
+    },
+    get: function(userId) {
+      for (var i = 0; i < users.length; i++) {
+        if (users[i].id === parseInt(userId)) {
+          return users[i];
+        }
+      }
+      return null;
+    }
+  };
+});
