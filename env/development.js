96518eebc5d15db853fa10d275f4239f3af234e2
annieredmond1 <annieredmond1@gmail.com>
2015-12-16 08:57:54 -0800

    changed test key

--- env/development.js
+++ env/development.js
@@ -5,5 +5,5 @@ module.exports = {
   port: port,
   db: 'mongodb://localhost/walkr-fit',
   TOKEN_SECRET: process.env.TOKEN_SECRET,
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET 
+  FACEBOOK_SECRET_TEST: process.env.FACEBOOK_SECRET_TEST
 };
