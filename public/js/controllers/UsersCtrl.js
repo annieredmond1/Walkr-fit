7d43cdebc4e4e4924f27813f9edbda3311e996d2
annieredmond1 <annieredmond1@gmail.com>
2015-12-17 09:05:06 -0800

    splash page styling

--- public/js/controllers/UsersCtrl.js
+++ public/js/controllers/UsersCtrl.js
@@ -4,15 +4,15 @@
 
 angular.module('walkr-fit')
   .controller('ProfileCtrl', ['Walk', '$scope', '$http', '$auth', 'Auth', '$location', function(Walk, $scope, $http, $auth, Auth, $location) {
+    console.log('profile ctrl active');
     $http.get('/api/me').then(function(data) {
       $scope.user = data.data;
-      console.log("heres scope data:", data.data);
-    console.log('user is: !!!!! ', $scope.user);
     });
     $scope.currentUser = Auth.currentUser;
     $scope.myWalks = Walk.myWalks();
     $scope.rsvpWalks = Walk.rsvpWalks();
     console.log('rsvpwalks is: ', $scope.rsvpwalks);
+
 
     $scope.walkShow = function(walk) {
       Walk.get({ id: walk._id }, function(walk) {
