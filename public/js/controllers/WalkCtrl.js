c4c1845e68bd4250b9c6e771dc00e457a984a4e3
annieredmond1 <annieredmond1@gmail.com>
2016-01-20 14:17:14 -0800

    heroku debug

--- public/js/controllers/WalkCtrl.js
+++ public/js/controllers/WalkCtrl.js
@@ -230,15 +230,14 @@ angular.module('walkr-fit')
   var indexOfCurrentUser;
 
   //add facebook share button to html
-  // $window.fbAsyncInit = function(){
-  //   console.log('fb async function run');
-  //   $('.fb-share-div').append('<div class="fb-share-button pull-right" data-href="https://walkr.fit" data-layout="icon_link"></div>');
-  //   FB.XFBML.parse();
-  //  };
+  $window.fbAsyncInit = function(){
+    console.log('fb async function run');
+    
+   };
 
   $(document).ready(function() {
     console.log('document ready');
-      $('.fb-share-div').append('<div class="fb-share-button pull-right" data-href="https://walkr.fit" data-layout="icon_link"></div>'); 
+      // $('.fb-share-div').append('<div class="fb-share-button pull-right" data-href="https://walkr.fit" data-layout="icon_link"></div>'); 
   });
 
   //set facebook share url to the current url
